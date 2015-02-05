aeab35f8790de6c01b31a08accbacaf0776002e2
Ian Miller <irmiller22@gmail.com>
2015-02-05 13:14:13 -0500

    updated spec

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -4,20 +4,21 @@ describe "Guessing CLI" do
 
   context 'user inputs' do
     it "responds to 'exit'" do
-      expect { exit_guessing_game }.to output(/Goodbye!/).to_stdout
+      expect(self).to receive(:gets).and_return('exit')
+      expect { run_guessing_game }.to output(/Goodbye!/).to_stdout
     end
 
     it "responds to a correct guess" do
-      allow(self).to receive(:random_number_generator).and_return(2)
-      expect(self).to receive(:gets).and_return("2").once
-      expect(self).to receive(:gets).and_return("exit").once
+      allow(self).to receive(:rand).and_return(2)
+      expect(self).to receive(:gets).and_return("2")
+      expect(self).to receive(:gets).and_return("exit")
       expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
     end
 
     it "responds to an incorrect guess" do
-      allow(self).to receive(:random_number_generator).and_return(5)
-      expect(self).to receive(:gets).and_return("2").once
-      expect(self).to receive(:gets).and_return("exit").once
+      allow(self).to receive(:rand).and_return(5)
+      expect(self).to receive(:gets).and_return("2")
+      expect(self).to receive(:gets).and_return("exit")
       expect { run_guessing_game }.to output(/The computer guessed 5./).to_stdout
     end
   end
