1d9222945c076736361c9596d7d05ef2f92c08fa
Ian Miller <irmiller22@gmail.com>
2015-02-05 12:57:34 -0500

    updated tests

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -7,17 +7,19 @@ describe "Guessing CLI" do
       expect { exit_guessing_game }.to output(/Goodbye!/).to_stdout
     end
 
-    # it "responds to a correct guess" do
-    #   allow(self).to receive(:random_number_generator).and_return("2")
-    #   allow(self).to receive(:gets).and_return("2")
-    #   expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
-    # end
+    it "responds to a correct guess" do
+      allow(self).to receive(:random_number_generator).and_return(2)
+      expect(self).to receive(:gets).and_return("2").once
+      expect(self).to receive(:gets).and_return("exit").once
+      expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
+    end
 
-    # it "responds to an incorrect guess" do
-    #   allow(self).to receive(:random_number_generator).and_return("5")
-    #   allow(self).to receive(:gets).and_return("2")
-    #   expect { run_guessing_game }.to output(/The computer guessed 5./).to_stdout
-    # end
+    it "responds to an incorrect guess" do
+      allow(self).to receive(:random_number_generator).and_return(5)
+      expect(self).to receive(:gets).and_return("2").once
+      expect(self).to receive(:gets).and_return("exit").once
+      expect { run_guessing_game }.to output(/The computer guessed 5./).to_stdout
+    end
   end
 
   it 'starts the script with the run_guessing_game method' do
